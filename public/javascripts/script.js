b0814ba4dee613297f8623fb840b91c424a8a132
Sumon <sumonkhan1081@gmail.com>
2023-10-01 17:20:01 +0530

    textarea data show feature complete

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -27,3 +27,4 @@ function createFileAndFolder() {
     })
 }
 createFileAndFolder();
+
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+// const { type } = require('os');//******** 
 
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  const filedata = fs.readdirSync('public/database',{withFileTypes:true},"utf-8");
+  const files = fs.readdirSync('public/database',{withFileTypes:true});
   res.render('index',{
-    filedata:filedata,
+    files:files,
+    fileInsidedata:null,
+    filename:null,
   });
 });
 
@@ -16,10 +19,23 @@ router.post('/createfile',(req,res)=>{
   fs.writeFileSync(`public/database/${req.body.filename}`,"");
   res.redirect('/');
 })
+
 //folder create feature
 router.post('/createfolder',(req,res)=>{
   fs.mkdirSync(`public/database/${req.body.foldername}`);
   res.redirect('/');
+})
+
+//file inside data show
+router.get('/show/:filename',(req,res)=>{
+  const files = fs.readdirSync('public/database',{withFileTypes:true});
+  const fileInsidedata = fs.readFileSync(`public/database/${req.params.filename}`,"utf-8")
+  console.log(fileInsidedata);
+  res.render('index',{
+    files:files,
+    fileInsidedata:fileInsidedata,
+    filename:req.params.filename,
+  });
 })
 
 module.exports = router;
