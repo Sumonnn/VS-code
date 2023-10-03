a02b2fd498de987071f44231fc3b4337d99ed8c9
Sumon <sumonkhan1081@gmail.com>
2023-10-04 00:13:11 +0530

    project done -- all feature done like innerData show and save besicaly CRUD-operation set

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
@@ -5,42 +6,57 @@ const fs = require('fs');
 
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  const files = fs.readdirSync('public/database',{withFileTypes:true});
-  res.render('index',{
-    files:files,
-    fileInsidedata:null,
-    filename:null,
+router.get('/', function (req, res) {
+  const files = fs.readdirSync('public/database', { withFileTypes: true });
+  res.render('index', {
+    files: files,
+    fileInsidedata: null,
+    filename: null,
   });
 });
 
 // file create feature
-router.post('/createfile',(req,res)=>{
-  fs.writeFileSync(`public/database/${req.body.filename}`,"");
+router.post('/createfile', (req, res) => {
+  fs.writeFileSync(`public/database/${req.body.filename}`, "");
   res.redirect('/');
 })
 
 //folder create feature
-router.post('/createfolder',(req,res)=>{
+router.post('/createfolder', (req, res) => {
   fs.mkdirSync(`public/database/${req.body.foldername}`);
   res.redirect('/');
 })
 
 //file inside data show
-router.get('/show/:filename',(req,res)=>{
-  const files = fs.readdirSync('public/database',{withFileTypes:true});
-  const fileInsidedata = fs.readFileSync(`public/database/${req.params.filename}`,"utf-8")
-  console.log(fileInsidedata);
-  res.render('index',{
-    files:files,
-    fileInsidedata:fileInsidedata,
-    filename:req.params.filename,
+router.get('/show/:filename', (req, res) => {
+  const files = fs.readdirSync('public/database', { withFileTypes: true });
+  const fileInsidedata = fs.readFileSync(`public/database/${req.params.filename}`, "utf-8");
+  // console.log(fileInsidedata);
+  res.render('index', {
+    files: files,
+    fileInsidedata: fileInsidedata,
+    filename: req.params.filename,
   });
 })
 
 //delete route
-router.get('/delete/:filename',(req,res)=>{
-  fs.unlinkSync(`public/database/${req.params.filename}`);
+router.get('/delete/:filename', (req, res) => {
+  fs.stat(`public/database/${req.params.filename}`, (err, checkDir) => {
+      if (checkDir.isDirectory()) {
+        fs.rmdirSync(`public/database/${req.params.filename}`);
+      }
+      else {
+        fs.unlinkSync(`public/database/${req.params.filename}`);
+      }
+  })
+  // console.log(req.params.filename)
   res.redirect('/');
+})
+
+//inside data update route
+router.post('/update/:filename',(req,res)=>{
+  const innerData = fs.readFileSync(`public/database/${req.params.filename}`,"utf-8");
+  fs.writeFileSync(`public/database/${req.params.filename}`,req.body.txtarea);
+  res.redirect(`/show/${req.params.filename}`);
 })
 module.exports = router;
